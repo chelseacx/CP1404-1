e2733b4d5ec3637c407fe8ca6a0e87b70cb7da25
chelseacx <chelsea.chen@my.jcu.edu.au>
2016-05-24 12:28:45 +0800

    Changed function name of get_dimensions to get_values. Fixed some PEP styling problems. Added docstring to get_values

--- calcAreaAndVolume.py
+++ calcAreaAndVolume.py
@@ -10,13 +10,16 @@ volume = width*height*depth
 print("area" ,area, "volume" ,volume)
 """
 
-def get_dimentions():
+
+def get_values():
+    """ function to get the length and width """
     length = int(input("Enter the length: "))
     width = int(input("Enter the width: "))
     return length, width
 
+
 def main():
-    length, width =get_dimentions()
+    length, width = get_values()
     area= length * width
     print(area)
 
